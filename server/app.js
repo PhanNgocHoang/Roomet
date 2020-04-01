f0bc9e7a0c648074a4464cc732d9e1a5c28233f2
Phan Ngoc  Hoang <hoangpngch17194@fpt.edu.vn>
2020-04-01 14:24:36 +0700

    test

--- server/app.js
+++ server/app.js
@@ -1,4 +1,4 @@
-const io = require('socket.io')(3000);
+const io = require('socket.io')(process.env.PORT ||3000);
 const arrUser = []
 io.on('connection', socket=>{
    socket.on('UserName', user=>{
